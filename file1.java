c5274bbd9075c92c1826d4ae08ee7aa0b2a9466d
rajendra <rajendrasanwal@gmail.com>
2019-01-09 16:21:45 +0530

    my work

--- file1.java
+++ file1.java
@@ -8,6 +8,6 @@ public class File1 {
 	public static void main(String[] args) {
 					System.out.format(" feture1 branch  changed");
 		System.out.format("changes in local brancgh");
-		System.out.format("added new by me...........");
+		System.out.format("local changes.......");
 	}
 }
